abb731063906cb08b5616947bc6eb7c2cc50dcb5
absolutnigilist <77591606+absolutnigilist@users.noreply.github.com>
2021-05-20 01:59:10 +0300

    Add files via upload

---
+++ hwcb_lesson3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+// проверка является ли число простым
+/*int main (){
+int a;
+int i;
+printf ("Enter your number: ");
+scanf ("%d", &a);
+for(i=2;i<a;i++){
+    if (a%i==0){
+    printf ("Number is not prime");
+    return 0;
+    }
+}
+printf ("Number is prime");
+return 0;
+}
+*/
+//цикл сложения цифр от 1 до 10
+/*int main ()
+{
+    int a=0;
+    int n=10;
+    int i;
+    for (i=1; i<=n; i++)
+        a=a+i;
+    printf ("%d", a);
+    return 0;
+}
+*/
